62e013d0dd2d7940121dbed4f1b5bd5bf41e0a93
Alpha <Alpha@administrator17>
2026-08-17 07:30:33 +0530

    hi

--- src/test/java/sample/Basic.java
+++ src/test/java/sample/Basic.java
@@ -2,8 +2,9 @@ package sample;
 
 public class Basic {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) 
+	{
+		System.out.println("Welcome");
 
 	}
 
